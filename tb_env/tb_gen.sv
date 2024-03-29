e03975929be7d6da6ff471c8b4964065551445e6
Zachary <z_zazzar@live.concordia.ca>
2024-03-28 20:40:34 -0400

    Created agent module
    Created tb_agt file & agent class, updated env.sv, small changes to tb_trans.sv, defs.sv

--- tb_env/tb_gen.sv
+++ tb_env/tb_gen.sv
@@ -5,8 +5,6 @@ Generator class that creates transaction objects then sends them to the agent cl
 Zachary Zazzara (40096894)
 
 Created on: March 28th, 2024
-Last edited: March 28th, 2024
-
 
 */
 
@@ -30,5 +28,52 @@ event ended;
 //transaction mailbox for generator -> agent
 mailbox #(tb_trans) gen2agt
 
+//Constructor
+function new(mailbox #(tb_trans) gen2agt, int max_trans_cnt);
+    this.gen2agt = gen2agt;
+    this.max_trans_cnt = max_trans_cnt;
+    rand_tr = new();
+endfunction
+
+task main();
+    $display($time, ": Starting apb_gen for %0d transactions", max_trans_cnt);
+    //This while loop runs until the randomly generated number of tests completes
+    while(!end_of_test())
+        begin
+            tb_trans temp_tr;
+
+            //wait & get transaction
+            temp_tr = get_transaction();
+
+            ++trans_cnt;
+
+            gen2agt.put(temp_tr)
+        end //end of while loop
+    $display($time, ": Ending tb_gen\n");
+
+    //Set the ended event flag to say the generator is finished
+    ->ended; 
+endtask
+
+//Function checks to see how many more transaction objects need to be generated before the test is complete
+//Same as in lab 4
+virtual function int end_of_test();
+	if(trans_cnt >= max_trans_cnt)
+		return 1;
+	else
+		return 0;
+endfunction
+
+//Returns a transaction object associated with tr member
+//Same as in lab 4
+virtual function tr_trans get_transaction();
+    rand_tr.trans_cnt = trans_cnt;
+    if (! this.rand_tr.randomize())
+      begin
+        $display("tb_gen::randomize failed");
+        $finish;
+      end
+    return rand_tr.copy();
+endfunction
 
 endclass
--- tb_env/tb_trans.sv
+++ tb_env/tb_trans.sv
@@ -1,11 +1,10 @@
 /*
 
-Transaction object class that should then be used by the generator class
+Transaction object class that should then be used by the generator class.
 
 Zachary Zazzara (40096894)
 
 Created on: March 27th, 2024
-Last edited: March 28th, 2024
 
 */
 
@@ -24,16 +23,21 @@ of those, uncomment lines marked with (&&) these lines, I guess.
 class tb_trans;
 	rand req_cmd_t cmd;
 	rand req_data_t op1;
-	rand req_data_t op2; //two datas for + and - ops, maybe replace this with something more elegant later?
-	rand req_tag_t tag;
+	rand req_data_t op2; //two datas for + and - ops, maybe apply constraints later?
+	rand req_port_t port; 
 	
-//Need contstrains for these randoms
-	constraint c_cmd {cmd inside {ADD, SUB, LSH, RSH};}
-	constraint c_tag {tag inside {2'b00, 2'b01, 2'b10, 2'b11};}
+	req_tag_t tag; //transaction tag to be assigned by the agent
+	
+	static int count = 0; //Counts the total number of transaction objects generatated so far
+	int id; //transaction ID of current transaction object/instance
+
+	//Need contstrains for these randoms
+	constraint c_cmd {cmd inside {ADD, SUB, LSH, RSH, NOP};}
+	constraint c_port {port inside {1,2,3,4};}
 	
 //Functions
 function new();
-
+this.id = count++;
 endfunction:new
 
 function void display();
@@ -42,13 +46,13 @@ function void display();
 endfunction :display
 
 function tb_trans copy(); //Deep copy function just in case we need it?
-	tb_trans cp = new();
-	cp.cmd = this.cmd;
-	cp.op1 = this.op1;
-	cp.op2 = this.op2;
-	cp.tag = this.tag;
-	copy = cp;
-	
+	tb_trans deep_cp = new();
+	deep_cp.cmd = this.cmd;
+	deep_cp.op1 = this.op1;
+	deep_cp.op2 = this.op2;
+	deep_cp.tag = this.tag;
+	deep_cp.port = this.port;
+	copy = deep_cp;
 endfunction:copy
 
 endclass
